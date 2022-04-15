7b9a072978e3af033872188d3d32b774d019eca6
Adelina Denysova <adochkadenisovaa@gmail.com>
2022-04-15 18:06:34 +0200

    add simple lightbox library and gallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,13 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+function onGalleryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    }).join('');
+};
+
+const galleryEL = document.querySelector('.gallery');
+galleryEL.insertAdjacentHTML('beforeend', onGalleryMarkup(galleryItems));
